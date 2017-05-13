3ccc1b4f4df8a57a18590d02826b63725339c306
Joern Bernhardt <jb@campudus.com>
2017-05-13 14:20:11 +0200

    Add some dependencies and test script
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-/**
- * Created by joern on 13/05/17.
- */
+const gulp = require("gulp");
+
+gulp.task("react", function () {
+  return reactBrowserify({
+    entries: ["js/ValveScout/index.js"],
+    debug: true,
+    extensions: ['.js', '.jsx']
+  })
+    .transform(babelify)
+    .bundle()
+    .on('error', errorLog)
+    .pipe(source('react_bundle.js'))
+    .pipe(process.env.NODE_ENV === "production" ? streamify(uglify()) : gutil.noop())
+    .pipe(gulp.dest("./build/js"))
+    .pipe(browserSync.reload({stream: true}));
+});
