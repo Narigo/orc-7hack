de39fba6857424839357dd5a4dcad1463cdbab09
konstantinaxt <axtkonstantin@hotmail.de>
2017-05-13 22:56:29 +0200

    add Question component, refactor some stuff

---
+++ src/domain/weg.de/components/Question/Question.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class Question extends Component {
+
+  render() {
+    const {children, title, description} = this.props;
+
+    return <div className="question">
+      <div className="title">{title}</div>
+      <div className="description">{description}</div>
+      {children}
+    </div>;
+  }
+}
