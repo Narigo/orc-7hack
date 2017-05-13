4860f2fc970db05513a0e85f7bf4eb8359cc3dc0
Joern Bernhardt <jb@campudus.com>
2017-05-13 18:21:59 +0200

    Add a few tests and ask function
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/finder.js
+++ src/finder.js
@@ -1,12 +1,40 @@
-
-export function finder(
+export const createFinder = (
   {
-    filter = {},
-    questions = {}
-  } = {}
-) {
+    filters = []
+  }
+) => {
 
-  const products = [];
-  return Promise.resolve(products);
+  return Promise
+    .resolve({
+      filters: filters.map(filter => ({
+        ...filter,
+        rating: 0.5
+      }))
+    });
 
-}
+};
+
+export const ask = (apiParams) => {
+  if (!apiParams || Object.keys(apiParams).length === 0) {
+    throw new Error("ask() needs an object of ApiParams.");
+  }
+
+  return ({question, answer}) => {
+    const params = {
+      ...apiParams,
+      filters: apiParams.filters.map(filter => {
+        const updateFn = question.filters[filter.name];
+        const rating = answer[filter.name] === (void 0) ? filter.rating : updateFn(filter.rating, answer[filter.name]);
+        return {
+          ...filter,
+          rating
+        };
+      })
+    };
+    return Promise.resolve(params);
+  }
+};
+
+export const result = (apiParams) => {
+  throw new Error("result() needs an object of ApiParams.");
+};
--- src/finder.spec.js
+++ src/finder.spec.js
@@ -1,15 +1,104 @@
 import expect from "must";
-import {finder} from "./finder";
+import {ask, createFinder, result} from "./finder";
+import createFinderSimpleFixture from "./__test__/createFinderSimpleFixture.json";
+import apiParamsMultipleFilters from "./__test__/apiParamsMultipleFilters.json";
 
-describe("The finder", () => {
+describe("createFinder", () => {
 
-  it("needs a filter configuration object", () => {
-    return expect(finder({})).to.reject.with.error(/filter/);
+  it("creates a state object", () => {
+    return createFinder({})
+      .then(apiParams => {
+        expect(apiParams).to.be.an.object();
+      });
   });
 
-  it("needs a questions configuration object", () => {
-    return expect(finder({})).to.reject.with.error(/questions/);
+  it("can create simple apiParams from a config", () => {
+    return createFinder({
+      filters: [
+        {
+          "name": "safety"
+        }
+      ]
+    })
+      .then(apiParams => {
+        expect(apiParams).to.eql(createFinderSimpleFixture);
+      });
   });
 
 });
 
+describe("ask", () => {
+
+  it("needs a state object", () => {
+    return expect(() => ask()).to.throw(/ApiParams/i);
+  });
+
+  it("can be used to increment a rating with a question/answer", () => {
+    const apiParams = apiParamsMultipleFilters;
+    const question = {
+      filters: {
+        safety: (currentRating, answerValue) => answerValue
+      }
+    };
+    const answer = {
+      safety: 1
+    };
+    return ask(apiParams)({
+      question,
+      answer
+    })
+      .then(resultApiParams => {
+        const safetyFilter = resultApiParams.filters.find(filter => filter.name === "safety");
+        expect(safetyFilter.rating).to.eql(1);
+      });
+  });
+
+  it("can be used to decrement a rating with a question/answer", () => {
+    const apiParams = apiParamsMultipleFilters;
+    const question = {
+      filters: {
+        activities: (currentRating, answerValue) => answerValue
+      }
+    };
+    const answer = {
+      activities: 0
+    };
+    return ask(apiParams)({
+      question,
+      answer
+    })
+      .then(resultApiParams => {
+        const activitiesFilter = resultApiParams.filters.find(filter => filter.name === "activities");
+        expect(activitiesFilter.rating).to.eql(0);
+      });
+  });
+
+  it("can be used to manipulate a rating with a question/answer function", () => {
+    const apiParams = apiParamsMultipleFilters;
+    const question = {
+      filters: {
+        activities: (currentRating, answerValue) => currentRating * answerValue
+      }
+    };
+    const answer = {
+      activities: 0.5
+    };
+    return ask(apiParams)({
+      question,
+      answer
+    })
+      .then(resultApiParams => {
+        const activitiesFilter = resultApiParams.filters.find(filter => filter.name === "activities");
+        expect(activitiesFilter.rating).to.eql(0.25);
+      });
+  });
+
+});
+
+describe("result", () => {
+
+  it("needs a state object", () => {
+    return expect(() => result()).to.throw(/ApiParams/i);
+  });
+
+});
