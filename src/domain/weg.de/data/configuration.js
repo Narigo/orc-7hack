51a0ff660dcccc3efa4c0e31d2703d6e3bcd0e18
Joern Bernhardt <jb@campudus.com>
2017-05-14 01:28:19 +0200

    Add some questions
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/domain/weg.de/data/configuration.js
+++ src/domain/weg.de/data/configuration.js
@@ -21,9 +21,48 @@ export const filters = [
   },
   {
     id: "activities",
-    map: (value) => {
+    map: (value, filters) => {
       return {
-        ...filters
+        ...filters,
+        categories: filters.categories.filter((category, idx) => {
+          return (value > 0.5 || idx < (filters.categories.length / 2));
+        })
+      }
+    }
+  },
+  {
+    id: "nightlife",
+    map: (value, filters) => {
+      return {
+        ...filters,
+
+      }
+    }
+  },
+  {
+    id: "cities",
+    map: (value, filters) => {
+      return {
+        ...filters,
+
+      }
+    }
+  },
+  {
+    id: "comfort",
+    map: (value, filters) => {
+      return {
+        ...filters,
+
+      }
+    }
+  },
+  {
+    id: "distance",
+    map: (value, filters) => {
+      return {
+        ...filters,
+
       }
     }
   }
@@ -31,12 +70,13 @@ export const filters = [
 
 export const questions = [
   {
-    kind: (props) => <RangeSlider {...props}/>,
+    kind: (props) => <RangeSlider {...props} />,
     title: "Wie sportlich bist Du?",
     description: "",
     filters: {
-      safety: weighted(0.3),
-      activities: weighted(1)
+      safety: weighted(0.2),
+      activities: weighted(1),
+      nightlife: weighted(0.5),
     },
     minValue: 0,
     maxValue: 1,
@@ -55,7 +95,7 @@ export const questions = [
     footer: value => (value < 0.2) ? <div>Faultier!</div> : (value > 0.8) ? <div>Jaroslav!</div> : null
   },
   {
-    kind: (props) => <RangeSlider {...props}/>,
+    kind: (props) => <RangeSlider {...props} />,
     title: "Wie risikobereit bist Du?",
     description: "",
     filters: {
@@ -69,5 +109,86 @@ export const questions = [
     minLabel: value => <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>Hasenfuß</div>,
     maxLabel: value => <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>Rambo</div>,
     footer: value => (value < 0.2) ? <div>Angsthase!</div> : null
+  },
+  {
+    kind: (props) => <RangeSlider {...props} />,
+    title: "Darf es auch mal lauter werden?",
+    description: "",
+    filters: {
+      safety: weighted(0.2),
+      activities: weighted(0.8),
+      nightlife: weighted(1)
+    },
+    minValue: 0,
+    maxValue: 1,
+    initialValue: 0.5,
+    step: 0.01,
+    minLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Ich mag es lieber ruhig.
+      </div>
+    ),
+    maxLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Ich tanze auf jeder Party.
+      </div>
+    ),
+    footer: value => (value < 0.2) ? <div>Streber!</div> : (value > 0.8) ? <div>Partytiger!</div> : null
+  },
+  {
+    kind: (props) => <RangeSlider {...props} />,
+    title: "Gefallen Dir ausgedehnte Shopping-Touren?",
+    description: "",
+    filters: {
+      safety: weighted(0.1),
+      nightlife: weighted(0.6),
+      cities: weighted(1),
+      comfort: (rating, answer) => weighted(0.5)(rating, 1 - answer) // inverse answer
+    },
+    minValue: 0,
+    maxValue: 1,
+    initialValue: 0.5,
+    step: 0.01,
+    minLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Ich hasse einkaufen.
+      </div>
+    ),
+    maxLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Ich liebe einkaufen!
+      </div>
+    ),
+    footer: value => (value < 0.2)
+      ? <div>Amazonbesteller</div>
+      : (value > 0.8)
+        ? <div>Wieso suchst Du dann online...?</div>
+        : null
+  },
+  {
+    kind: (props) => <RangeSlider {...props} />,
+    title: "Lernst Du gerne fremde Kulturen kennen?",
+    description: "",
+    filters: {
+      safety: weighted(0.2),
+      cities: weighted(1),
+      comfort: (rating, answer) => weighted(0.8)(rating, 1 - answer),
+      distance: (rating, answer) => weighted(1)(rating, 1 - answer) // TODO inverseWeighted function
+    },
+    minValue: 0,
+    maxValue: 1,
+    initialValue: 0.5,
+    step: 0.01,
+    minLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Das Bekannte gefällt mir.
+      </div>
+    ),
+    maxLabel: value => (
+      <div className={value < 0.2 ? "unsure" : value > 0.8 ? "confident" : ""}>
+        Jeden Tag was Neues!
+      </div>
+    ),
+    footer: () => <div>...</div>
   }
 ];
