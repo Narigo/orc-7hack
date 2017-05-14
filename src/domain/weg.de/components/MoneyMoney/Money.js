75c143262cb778e80c7da614153c2212ecce3cd0
Joern Bernhardt <jb@campudus.com>
2017-05-14 11:15:54 +0200

    Sort by stars and add key to money
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/domain/weg.de/components/MoneyMoney/Money.js
+++ src/domain/weg.de/components/MoneyMoney/Money.js
@@ -20,8 +20,8 @@ export default class Money extends Component {
       <div className="money" onClick={() => this.handleClick()}>
         {(this.state.moneyBag.length > 8)
           ? <img src="https://emojipedia-us.s3.amazonaws.com/cache/ae/f6/aef6f2d4c9b96d79b69a2350a92da8ba.png" />
-          : this.state.moneyBag.map(() =>
-            <img src="https://emojipedia-us.s3.amazonaws.com/cache/9e/de/9ede0ba323807d2bf1e06ebfecfcdff8.png" />
+          : this.state.moneyBag.map((bag, idx) =>
+            <img key={idx} src="https://emojipedia-us.s3.amazonaws.com/cache/9e/de/9ede0ba323807d2bf1e06ebfecfcdff8.png" />
           )}
       </div>
     )
