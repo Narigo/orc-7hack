8a8bcc6766958cf47318e5cb5b12a820aaa87342
Joern Bernhardt <jb@campudus.com>
2017-05-14 11:11:02 +0200

    Refactor money
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/domain/weg.de/components/MoneyMoney/Money.js
+++ src/domain/weg.de/components/MoneyMoney/Money.js
@@ -5,45 +5,24 @@ export default class Money extends Component {
   constructor() {
     super();
     this.state = {
-      money: 0,
-      count: 0,
       moneyBag: ["💰"]
     };
   }
 
-  handleClick = (value) => {
-    if (this.state.count < 8) {
-      this.state.moneyBag.push("💰");
-    }
+  handleClick() {
     this.setState({
-      money: value,
-      count: this.state.count + 1,
-      moneyBag: this.state.moneyBag
+      moneyBag: [...this.state.moneyBag, "💰"]
     });
-    if (this.state.count > 9) {
-      this.setState(
-        {
-          moneyBag: ["💰"],
-          count: 1
-        });
-    }
   }
 
   render() {
-    // const {children, title, description} = this.props;
-    console.log("moneybag: ", this.state.moneyBag);
-
     return (
-      // 9
-      <div className="money" onClick={() => this.handleClick(this.state.money + 5)}>
-        { this.state.moneyBag.map(() => {
-          if (this.state.count < 8) {
-            return <img src="https://emojipedia-us.s3.amazonaws.com/cache/9e/de/9ede0ba323807d2bf1e06ebfecfcdff8.png" />
-          } else {
-            return <img src="https://emojipedia-us.s3.amazonaws.com/cache/ae/f6/aef6f2d4c9b96d79b69a2350a92da8ba.png" />
-          }
-        })
-        }
+      <div className="money" onClick={() => this.handleClick()}>
+        {(this.state.moneyBag.length > 8)
+          ? <img src="https://emojipedia-us.s3.amazonaws.com/cache/ae/f6/aef6f2d4c9b96d79b69a2350a92da8ba.png" />
+          : this.state.moneyBag.map(() =>
+            <img src="https://emojipedia-us.s3.amazonaws.com/cache/9e/de/9ede0ba323807d2bf1e06ebfecfcdff8.png" />
+          )}
       </div>
     )
       ;
